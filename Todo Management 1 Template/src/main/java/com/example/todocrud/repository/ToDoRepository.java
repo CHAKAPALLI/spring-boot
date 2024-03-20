9875584030bb633cfb5655a4f48dc7425e95dd92
CHAKAPALLI <vivekchakapalli9@gmail.com>
2024-03-20 16:06:15 +0530

    added

--- Todo Management 1 Template/src/main/java/com/example/todocrud/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todocrud.repository;
-
-import com.example.todocrud.entity.Todo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ToDoRepository extends CrudRepository<Todo,Long> {
-}
--- Todo Management 1 Template/src/main/java/com/example/todocrud/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todocrud.repository;
-
-import com.example.todocrud.entity.Users;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Users,Long> {
-}
